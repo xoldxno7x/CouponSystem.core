a0adc237e3d29d24e299f1d7e6c01d2e8aa59a01
xoldxno7x <36238563+xoldxno7x@users.noreply.github.com>
2018-03-08 12:02:23 +0200

    Delete CouponsApplication.java

--- CuponHandling/coupons.web/src/coupons/web/CouponsApplication.java
+++
@@ -1,9 +0,0 @@
-package coupons.web;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class CouponsApplication extends Application {
-
-}
