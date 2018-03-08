5f229f2fb2704b384d5b8cff9386cfff26eab125
xoldxno7x <36238563+xoldxno7x@users.noreply.github.com>
2018-03-08 12:07:42 +0200

    Delete CouponApplication.java

--- CuponHandling/CouponWebREST/src/coupon/service/CouponApplication.java
+++
@@ -1,9 +0,0 @@
-package coupon.service;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class CouponApplication extends Application {
-
-}
